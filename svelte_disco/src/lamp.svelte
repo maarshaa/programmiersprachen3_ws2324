e58a0b992b88a2355a6eeea4be283f2132e54273
maarshaa <120369843+maarshaa@users.noreply.github.com>
2023-12-11 15:49:33 +0100

    hj

--- svelte_disco/src/lamp.svelte
+++ svelte_disco/src/lamp.svelte
@@ -1,7 +1,7 @@
 <script>
     export let left;
     export let top;
-    let color= 'rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})';
+    let color= "rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})";
 </script>
 
 <div class="lamp" style="background-color: {color}; left:{left}px; top:{top}px"></div>
